8e601e054eafd88192c7d9576bbfa9435f9dd396
umarfarooq1 <ufarooq517659@gmail.com>
2017-07-14 03:55:26 +0500

    extracting data from bluekai

--- background.js
+++ background.js
@@ -10,24 +10,91 @@ function GetDate(){
   newdate = year + "/" + month + "/" + day;
   return newdate
 }
+function GetDiff(d1,d2){
+  var date1 = new Date(d1);
+  var date2 = new Date(d2);
+  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+  return diffDays;
+}
+
 /*chrome.windows.getAll({"populate" : true}, function(myarr){
   console.log(myarr)
 })*/
-chrome.storage.sync.get('extensionDate', function(result){
-  console.log("The old collection time is "+result.extensionDate)
-  console.log("The current time is "+GetDate())
-  if (result.extensionDate !== GetDate()) { //need to decide on the next snapshot time lapse
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){  
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
+var requestFilter = {
+    urls: ["<all_urls>"]
+},
+
+extraInfoSpec = ['requestHeaders', 'blocking'],
+handler = function(details) {
+
+var isRefererSet = false;
+var headers = details.requestHeaders,
+    blockingResponse = {};
+
+for (var i = 0, l = headers.length; i < l; ++i) {
+    if (headers[i].name == 'Referer') {
+        headers[i].value = "http://www.bluekai.com/registry";
+        isRefererSet = true;
+        break;
+    }
+}
+
+if (!isRefererSet) {
+    headers.push({
+        name: "Referer",
+        value: "http://www.bluekai.com/registry"
+    });
+}
+
+blockingResponse.requestHeaders = headers;
+return blockingResponse;
+};
+
+var dummy = function(tabId, changeInfo, tab){
+  //chrome.windows.getAll(function(win_list){
+    chrome.storage.sync.get('extensionDate', function(result){
+      if (GetDiff(result.extensionDate,GetDate()) === 0){ //&& win_list.length === 1
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          var activeTab = tabs[0];
+          console.log(activeTab);
+          chrome.webRequest.onBeforeSendHeaders.addListener(handler, requestFilter, extraInfoSpec);
           chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+        });
+      chrome.storage.sync.set({'extensionDate': GetDate()}, function() {
+        console.log("Updated collection time to "+GetDate())  
       });
-    });
-    chrome.storage.sync.set({'extensionDate': GetDate()}, function() {
-      console.log("Updated collection time to "+GetDate())  
-    });
-  }
-})
+     }
+   })
+  //});
+  cleanup();
+}
+var myfunc = function() {
+  chrome.storage.sync.get('extensionDate', function(result){
+    console.log("The old collection time is "+result.extensionDate)
+    console.log("The current time is "+GetDate())
+    console.log(GetDiff(result.extensionDate,GetDate()) === 0)
+    chrome.tabs.onUpdated.addListener(dummy);
+    /*if (GetDiff(result.extensionDate,GetDate()) === 0){//(result.extensionDate !== GetDate()) { //need to decide on the next snapshot time lapse
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          var activeTab = tabs[0];
+          console.log(activeTab);
+          chrome.webRequest.onBeforeSendHeaders.addListener(handler, requestFilter, extraInfoSpec);
+          chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+        });
+      chrome.storage.sync.set({'extensionDate': GetDate()}, function() {
+        console.log("Updated collection time to "+GetDate())  
+      });
+    }*/
+  })
+
+}
+chrome.windows.onCreated.addListener(myfunc);
+
+var cleanup = function() {
+    chrome.windows.onCreated.removeListener(myfunc);
+    chrome.tabs.onUpdated.removeListener(dummy);
+}
 
 
 // This block is new!
--- content.js
+++ content.js
@@ -3,6 +3,18 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
     	console.log(request);
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', "//tags.bluekai.com/registry?js=1&fg=58595b&fpfg=7d7d7d&font=arial&size=11&fpfont=arial&fpsize=9&lo=1", true);
+      xhr.send();
+       
+      xhr.onreadystatechange = processRequest;
+      function processRequest(e) {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+              var response = xhr.responseText;
+              console.log(response)
+              //alert(response.ip);
+          }
+      }
      // var firstHref = $("a[href^='http']").eq(0).attr("href");
      //console.log('hello from the content side')
       //console.log(firstHref);
