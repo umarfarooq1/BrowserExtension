a10d385c3946bbcea72b20d66236672fb3548d51
umarfarooq1 <ufarooq517659@gmail.com>
2017-09-03 01:33:27 +0500

    fixed minor bugs

--- background.js
+++ background.js
@@ -6,6 +6,17 @@ var checker = true;
 var timeGap = 0; //ensure this is set to the desired value before the extension is deployed
 var responses = 0;
 var toServer = {}
+var firstTime = false
+var firstTab = false
+var complete = false
+//var startUp = false
+chrome.storage.sync.get('extensionDate', function(result){ //need to add exception if "extensionDate" variable doesnt exist
+  if (result.extensionDate === undefined) {
+      chrome.storage.sync.set({'extensionDate': GetDate()}, function() {console.log('setting extensionDate variable')})                        
+      firstTime = true
+      console.log("I have installed the extension for the first time")
+  }
+})
 function GetDate(){
   var dateObj = new Date();
   var month = dateObj.getUTCMonth() + 1; //months from 1-12
@@ -58,14 +69,22 @@ handler = function(details) {
 
 var dummy = function(tabId, changeInfo, tab){
   chrome.storage.sync.get('extensionDate', function(result){ //need to add exception if "extensionDate" variable doesnt exist
-    console.log(result)
+    /*console.log(result)
+    console.log('I am inside dummy')
+    console.log(tab)
+    console.log(tabId)
+    console.log(changeInfo)*/
     if (GetDiff(result.extensionDate) >= timeGap && checker){ //&& win_list.length === 1
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         var activeTab = tabs[0];
-        console.log(activeTab);
+        //console.log(activeTab);
         chrome.webRequest.onBeforeSendHeaders.addListener(handler, requestFilter, extraInfoSpec);
-        if(activeTab.status ==="complete"){
-          chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});  
+        if(activeTab !==undefined){
+          if(activeTab.status ==="complete"){
+            //console.log('i made it here')
+            //console.log(activeTab)
+            chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});  
+          }  
         }
       });
    }
@@ -76,7 +95,10 @@ var dummy = function(tabId, changeInfo, tab){
 //chrome.windows.onCreated.addListener(myfunc);
 chrome.runtime.onStartup.addListener(function() {
   console.log('this is startup..')
-  chrome.tabs.onUpdated.addListener(dummy);
+  if (firstTab === false){
+    chrome.tabs.onUpdated.addListener(dummy);
+    firstTab = true
+  }
 });
 
 var BrowsingHist = new Promise (function(resolve,reject) {
@@ -98,22 +120,25 @@ chrome.runtime.onMessage.addListener(
     //if(request.text === "what is my tab_id?"){
       //console.log(sender.tab.id)
     //}
-    if( request.message === "ALL DONE") {
+    if(complete === false){
+      if( request.message === "ALL DONE" && toServer[request.type] === undefined) {
       responses++;
       toServer[request.type] = request.data
-    }
-    if (responses === 5) { //need to set this threshold,currently waiting for four responses. 0,1,2,3
-      chrome.storage.sync.set({'extensionDate': GetDate()}, function() {
-        BrowsingHist.then(function(data){
-          toServer['BrowsingHistory'] = data;
-          //console.log(new Set(toServer.googleSearchTerms))
-          console.log(toServer)
-          console.log(sender.tab.id)
-          console.log("ALL DONE. Updated collection time to "+GetDate())
-        })
-        chrome.tabs.onUpdated.removeListener(dummy);
-        checker = false;
-      });
+      }
+      if (responses === 5) { //need to set this threshold,currently waiting for four responses. 0,1,2,3
+        chrome.storage.sync.set({'extensionDate': GetDate()}, function() {
+          BrowsingHist.then(function(data){
+            toServer['BrowsingHistory'] = data;
+            //console.log(new Set(toServer.googleSearchTerms))
+            console.log(toServer)
+            console.log(sender.tab.id)
+            console.log("ALL DONE. Updated collection time to "+GetDate())
+          })
+          chrome.tabs.onUpdated.removeListener(dummy);
+          checker = false;
+          complete = true
+        });
+      }
     }
   }
 );
