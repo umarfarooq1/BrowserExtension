635046b641f70cad124a69f0df6ec002e6b9e2c6
umarfarooq1 <ufarooq517659@gmail.com>
2017-09-17 12:01:12 +0500

    server code

---
+++ server.js
@@ -0,0 +1,37 @@
+
+http = require('http');
+const fs = require('fs');
+server = http.createServer( function(req, res) {
+    console.dir(req.param);
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', function () {
+        	toServer = JSON.parse(body);
+        	console.log(toServer.BlueKai!==undefined,"BLUEKAI")
+        	console.log(toServer.BlueKai!==undefined,"FBadvertisers")
+        	console.log(toServer.BlueKai!==undefined,"FBinterests")
+        	console.log(toServer.BlueKai!==undefined,"googleAdSettings")
+        	console.log(toServer.BlueKai!==undefined,"exelate")
+        	console.log(toServer.BlueKai!==undefined,"googleSearchTerms")
+        	fs.writeFile('2pac.txt', body, (err) => {  
+			    // throws an error, you could also catch it here
+			    if (err) throw err;
+
+			    // success case, the file was saved
+			    console.log('Lyric saved!');
+			});
+        	
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+    }
+});
+
+port = 3000;
+host = '127.0.0.1';
+server.listen(port, host);
+console.log('Listening at http://' + host + ':' + port);
