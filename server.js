ec4315e8e9070de681f157b9afb0d62e2ecbb681
root <ufarooq517679@gmail.com>
2017-09-23 14:33:46 -0400

    added ssh

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 
-http = require('http');
+
+https = require('https');
 const fs = require('fs');
-server = http.createServer( function(req, res) {
+
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/osnproject.ccs.neu.edu/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/osnproject.ccs.neu.edu/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/osnproject.ccs.neu.edu/chain.pem')
+};
+
+server = https.createServer(options,function(req, res) {
     console.dir(req.param);
     if (req.method == 'POST') {
         console.log("POST");
@@ -17,7 +25,7 @@ server = http.createServer( function(req, res) {
         	console.log(toServer.BlueKai!==undefined,"googleAdSettings")
         	console.log(toServer.BlueKai!==undefined,"exelate")
         	console.log(toServer.BlueKai!==undefined,"googleSearchTerms")
-        	fs.writeFile('2pac.txt', body, (err) => {  
+        	fs.writeFile('2pac.txt', body, (err) => {
 			    // throws an error, you could also catch it here
 			    if (err) throw err;
 
@@ -33,7 +41,7 @@ server = http.createServer( function(req, res) {
     }
 });
 
-port = 3000;
+port = 443;
 host = '129.10.115.133';
 server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
+console.log('Listening at https://' + host + ':' + port);
