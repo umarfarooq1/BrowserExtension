f20229a3d891bf7429a0332e07f370bd0e1a1ad4
umarfarooq1 <ufarooq517679@gmail.com>
2017-09-24 08:57:06 -0400

    saving user data and setting session

--- server.js
+++ server.js
@@ -8,24 +8,20 @@ var options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/osnproject.ccs.neu.edu/cert.pem'),
   ca: fs.readFileSync('/etc/letsencrypt/live/osnproject.ccs.neu.edu/chain.pem')
 };
-
+function ID() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
 server = https.createServer(options,function(req, res) {
-    console.dir(req.param);
     if (req.method == 'POST') {
         console.log("POST");
         var body = '';
         req.on('data', function (data) {
             body += data;
         });
+	var id = ID();
         req.on('end', function () {
         	toServer = JSON.parse(body);
-        	console.log(toServer.BlueKai!==undefined,"BLUEKAI")
-        	console.log(toServer.BlueKai!==undefined,"FBadvertisers")
-        	console.log(toServer.BlueKai!==undefined,"FBinterests")
-        	console.log(toServer.BlueKai!==undefined,"googleAdSettings")
-        	console.log(toServer.BlueKai!==undefined,"exelate")
-        	console.log(toServer.BlueKai!==undefined,"googleSearchTerms")
-        	fs.writeFile('2pac.txt', body, (err) => {
+        	fs.writeFile('/home/ufarooq/BrowserExtension/User_Data/'+id+'.txt', body, (err) => {
 			    // throws an error, you could also catch it here
 			    if (err) throw err;
 
@@ -34,7 +30,7 @@ server = https.createServer(options,function(req, res) {
 			});
         });
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
+        res.end(id);
     }
     else{
 	    res.end("not now")
