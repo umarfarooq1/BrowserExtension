21aba606a9999dac1901fc392b108c0382d58234
umarfarooq1 <ufarooq517679@gmail.com>
2017-09-30 09:10:20 -0400

    fixed bugs

--- query.py
+++ query.py
@@ -2,16 +2,18 @@ import mysql.connector,os,json
 
 path = '/home/ufarooq/BrowserExtension/User_Data'
 users = os.listdir(path)
-query = "INSERT INTO TABLE "
-for i in users:
+query = "INSERT INTO "
+queries = []
 
+cnx = mysql.connector.connect(user='root',password = 'umarfarooq',database='ExtensionData')
+cursor = cnx.cursor()
+
+for i in users:
 	f = open(path+'/'+i)
 	data = f.read()
 	uid = i.split('1')[0][:-1]
 	timestamp = i.split('_')[2].replace('.txt','')
-#	print uid,timestamp
 	jsonData = json.loads(data)
-#	print jsonData.keys()
 	exelate = jsonData['exelate']
 	bluekai = jsonData['BlueKai']
 	browsingHist = jsonData['BrowsingHistory']
@@ -24,12 +26,13 @@ for i in users:
 	if type(exelate) is not list:
 		print "insert in Error table"
 	else:
+		exelate = ['shopping']
 		if len(exelate) != 0:
-			query1 = query+"USER_Exelate (USER_ID, Interest_DESC) VALUES "
+			query1 = query+"USER_Exelate (USER_ID,shot_timestamp, Interest_DESC) VALUES "
 			for j in exelate:
-				query1 = query1+ str((uid,j))+", "
+				query1 = query1+ str((uid,str(timestamp),j))+", "
 			query1 = query1[:-2]+";"
-#		print query1
+			queries.append(query1)
 	if type(googAd) is not list:
                 print "insert in Error table"
         else:
@@ -38,7 +41,7 @@ for i in users:
                         for j in googAd:
                                 query2 = query2+ str((uid,j))+", "
                         query2 = query2[:-2]+";"
-#                print query2
+#			queries.append(query2)
 	if type(bluekai) is not list:
                 print "insert in Error table"
         else:
@@ -47,7 +50,7 @@ for i in users:
                         for j in bluekai:
                                 query3 = query3+ str((j['id'],j['img'],j['fimg'],'NULL','NULL'))+", "
 			query3 = query3[:-2]+";"
-#                print query3
+#			queries.append(query3)
 	if type(browsingHist) is not list:
                 print "insert in Error table"
         else:
@@ -56,7 +59,7 @@ for i in users:
 			for j in browsingHist:
                                 query4 = query4+ str((uid,j['id'],j['lastVisitTime'],j['title'],j['url']))+", "
                         query4 = query4[:-2]+";"
-#                print query4
+#			queries.append(query4)
 	if type(googSearch) is not list:
                 print "insert in Error table"
         else:
@@ -70,25 +73,28 @@ for i in users:
 				else:
 					query5 = query5+ str((uid,"NULL",j[0],"NULL",j[1]))+", "
                         query5 = query5[:-2]+";"
-#                print query5
+#			queries.append(query5)
 	try:
 		fbInterests['Error']
 		print "insert in Error table"
 	except KeyError:
 		query6 = query+"USER_FB_INTERESTS (USER_ID, Description, disambiguation_category, FBID, IMAGE_URL,NAME,Subtype,Topic,TYPE) VALUES "
+		test = query6
 		for j in fbInterests['interests']:
 			query6 = query6+ str((uid,j['description'],j['disambiguation_category'],j['fbid'],j['image_url'],j['name'],j['subtype'],j['topic'],j['type']))+", "
 		for j in fbInterests['removed_interests']:
                         query6 = query6+ str((uid,j['description'],j['disambiguation_category'],j['fbid'],j['image_url'],j['name'],j['subtype'],j['topic'],j['type']))+", "
 		for j in fbInterests['suggested_interests']:
                         query6 = query6+ str((uid,j['description'],j['disambiguation_category'],j['fbid'],j['image_url'],j['name'],j['subtype'],j['topic'],j['type']))+", "
-		query6 = query6[:-2]+";"
-#		print query6
+		if query6 != test:
+			query6 = query6[:-2]+";"
+#			queries.append(query6)
         try:
                 fbAdvert['Error']
                 print "insert in Error table"
         except KeyError:
 		query7 = query+"USER_FB_ADS (USER_ID, ca_type, FBID, IMAGE_URL,NAME,REMOVED,report_date,SOURCE_URL) VALUES "
+		test = query7
 		fbAdvert = fbAdvert['advertisers']
 		for j in fbAdvert['clicked']:
 			query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],j['removed'],j['report_date'],j['source_url']))+", "
@@ -100,22 +106,23 @@ for i in users:
                         query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],j['removed'],j['report_date'],j['source_url']))+", "
 		for j in fbAdvert['website_app']:
                         query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],"NULL","NULL",j['source_url']))+", "
-		query7 = query7[:-2]+";"
-#		print query7
+		if test!=query7:
+			query7 = query7[:-2]+";"
+#			queries.append(query7)
         try:
                 survey['Error']
                 print "insert in Error table"
         except KeyError:
-		 query8 = query+"USER_SURVEY (USER_ID,RESPONSE) VALUES " + str((uid,survey))+";"
-#		 print query8
-#                print "regular"
+		 query8 = query+"USER_SURVEY (USER_ID,RESPONSE) VALUE " + str((uid,survey))+";"
+#		 queries.append(query8)
 
-
-'''
-cnx = mysql.connector.connect(user='root',password = 'umarfarooq',database='ExtensionData')
-cursor = cnx.cursor()
-cursor.execute('show databases;')
-for i in cursor:
-	print i
+#	print len(queries)
+	print queries
+	for q in queries:
+		print "I wash ehre"
+		cursor.execute(q)
+	queries = []
+cnx.commit()
 cursor.close()
-cnx.close()'''
+cnx.close()
+
