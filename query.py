8b1675ca1b68524d32a04959544ec7f81276cd47
root <ufarooq517679@gmail.com>
2017-09-30 11:51:41 -0400

    fixed bugs in db insertion

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-import mysql.connector,os,json
+import mysql.connector,os,json,ast
 
 path = '/home/ufarooq/BrowserExtension/User_Data'
 users = os.listdir(path)
@@ -26,20 +26,19 @@ for i in users:
 	if type(exelate) is not list:
 		print "insert in Error table"
 	else:
-		exelate = ['shopping']
 		if len(exelate) != 0:
 			query1 = query+"USER_Exelate (USER_ID,shot_timestamp, Interest_DESC) VALUES "
 			for j in exelate:
 				query1 = query1+ str((uid,str(timestamp),j))+", "
 			query1 = query1[:-2]+";"
-			queries.append(query1)
+#			queries.append(query1)
 	if type(googAd) is not list:
                 print "insert in Error table"
         else:
 		if len(googAd) != 0:
-			query2 = query+"USER_Google_ADS (USER_ID, AD_DESC) VALUES "
+			query2 = query+"USER_Google_ADS (USER_ID,shot_timestamp, AD_DESC) VALUES "
                         for j in googAd:
-                                query2 = query2+ str((uid,j))+", "
+                                query2 = query2+ str((uid,str(timestamp),str(j)))+", "
                         query2 = query2[:-2]+";"
 #			queries.append(query2)
 	if type(bluekai) is not list:
@@ -48,44 +47,48 @@ for i in users:
                 if len(bluekai) != 0:
 			query3 = query+"BlueKai (IMAGE_ID, IMAGE_title_url, IMAGE_desc_url, IMAGE_title_text, IMAGE_desc_text) VALUES "
                         for j in bluekai:
-                                query3 = query3+ str((j['id'],j['img'],j['fimg'],'NULL','NULL'))+", "
+				bid = j['img']
+				bid =  bid.split('/')[-1]
+				bid = bid.split('_')[0]
+                                query3 = query3+ str((int(bid),str(j['img']),str(j['fimg']),'NULL','NULL'))+", "
 			query3 = query3[:-2]+";"
 #			queries.append(query3)
 	if type(browsingHist) is not list:
                 print "insert in Error table"
         else:
 		if len(browsingHist) != 0:
-			query4 = query+"USER_BROWSING_HISTORY (USER_ID, ID, TIMESTAMP, TITLE, URL) VALUES "
+			query4 = query+"USER_BROWSING_HISTORY (USER_ID,shot_timestamp, ID, TIMESTAMP, TITLE, URL) VALUES "
 			for j in browsingHist:
-                                query4 = query4+ str((uid,j['id'],j['lastVisitTime'],j['title'],j['url']))+", "
+#				print (uid,timestamp,j['id'],j['lastVisitTime'],j['title'],j['url'])
+                                query4 = query4+ str((str(uid),str(timestamp),int(j['id']),str(j['lastVisitTime']),str(j['title'].encode('utf-8')),str(j['url'])))+", "
                         query4 = query4[:-2]+";"
 #			queries.append(query4)
 	if type(googSearch) is not list:
                 print "insert in Error table"
         else:
 		if len(googSearch) != 0:
-			query5 = query+"Google_SearchTerms (USER_ID, SEARCH_TERM, CATEGORY, URL, TIMESTAMP) VALUES "
+			query5 = query+"Google_SearchTerms (USER_ID,shot_timestamp, SEARCH_TERM, CATEGORY, URL, TIMESTAMP) VALUES "
                         for j in googSearch:
 				if len(j) == 5:
-        	                        query5 = query5+ str((uid,j[0],j[2],j[3],j[4]))+", "
+        	                        query5 = query5+ str((uid,str(timestamp),str(j[0].encode('utf-8')),str(j[2]),str(j[3].encode('utf-8')),str(j[4])))+", "
 				elif len(j) == 4:
-					query5 = query5+ str((uid,j[0],j[2],"NULL",j[3]))+", "
+					query5 = query5+ str((uid,str(timestamp),str(j[0].encode('utf-8')),str(j[2]),"NULL",str(j[3])))+", "
 				else:
-					query5 = query5+ str((uid,"NULL",j[0],"NULL",j[1]))+", "
+					query5 = query5+ str((uid,str(timestamp),"NULL",str(j[0]),"NULL",str(j[1])))+", "
                         query5 = query5[:-2]+";"
 #			queries.append(query5)
 	try:
 		fbInterests['Error']
 		print "insert in Error table"
 	except KeyError:
-		query6 = query+"USER_FB_INTERESTS (USER_ID, Description, disambiguation_category, FBID, IMAGE_URL,NAME,Subtype,Topic,TYPE) VALUES "
+		query6 = query+"USER_FB_INTERESTS (USER_ID,shot_timestamp, Description, disambiguation_category, FBID, IMAGE_URL,NAME,Subtype,Topic,TYPE) VALUES "
 		test = query6
 		for j in fbInterests['interests']:
-			query6 = query6+ str((uid,j['description'],j['disambiguation_category'],j['fbid'],j['image_url'],j['name'],j['subtype'],j['topic'],j['type']))+", "
+			query6 = query6+ str((uid,str(timestamp),str(j['description'].encode('utf-8')),str(j['disambiguation_category']),int(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['subtype']),str(j['topic'].encode('utf-8')),str(j['type'])))+", "
 		for j in fbInterests['removed_interests']:
-                        query6 = query6+ str((uid,j['description'],j['disambiguation_category'],j['fbid'],j['image_url'],j['name'],j['subtype'],j['topic'],j['type']))+", "
+                        query6 = query6+ str((uid,str(timestamp),str(j['description'].encode('utf-8')),str(j['disambiguation_category']),int(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['subtype']),str(j['topic'].encode('utf-8')),str(j['type'])))+", "
 		for j in fbInterests['suggested_interests']:
-                        query6 = query6+ str((uid,j['description'],j['disambiguation_category'],j['fbid'],j['image_url'],j['name'],j['subtype'],j['topic'],j['type']))+", "
+                        query6 = query6+ str((uid,str(timestamp),str(j['description'].encode('utf-8')),str(j['disambiguation_category']),int(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['subtype']),str(j['topic'].encode('utf-8')),str(j['type'])))+", "
 		if query6 != test:
 			query6 = query6[:-2]+";"
 #			queries.append(query6)
@@ -93,19 +96,19 @@ for i in users:
                 fbAdvert['Error']
                 print "insert in Error table"
         except KeyError:
-		query7 = query+"USER_FB_ADS (USER_ID, ca_type, FBID, IMAGE_URL,NAME,REMOVED,report_date,SOURCE_URL) VALUES "
+		query7 = query+"USER_FB_ADS (USER_ID,shot_timestamp, ca_type, FBID, IMAGE_URL,NAME,REMOVED,report_date,SOURCE_URL) VALUES "
 		test = query7
 		fbAdvert = fbAdvert['advertisers']
 		for j in fbAdvert['clicked']:
-			query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],j['removed'],j['report_date'],j['source_url']))+", "
+			query7 = query7+str((uid,str(timestamp),str(j['ca_type']),str(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['removed']),str(j['report_date']),str(j['source_url'])))+", "
 		for j in fbAdvert['contact_info']:
-                        query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],j['removed'],j['report_date'],j['source_url']))+", "
+                        query7 = query7+str((uid,str(timestamp),str(j['ca_type']),str(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['removed']),str(j['report_date']),str(j['source_url'])))+", "
 		for j in fbAdvert['hidden']:
-                        query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],j['removed'],j['report_date'],j['source_url']))+", "
+                        query7 = query7+str((uid,str(timestamp),str(j['ca_type']),str(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['removed']),str(j['report_date']),str(j['source_url'])))+", "
 		for j in fbAdvert['store_visit']:
-                        query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],j['removed'],j['report_date'],j['source_url']))+", "
+                        query7 = query7+str((uid,str(timestamp),str(j['ca_type']),str(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),str(j['removed']),str(j['report_date']),str(j['source_url'])))+", "
 		for j in fbAdvert['website_app']:
-                        query7 = query7+str((uid,j['ca_type'],j['fbid'],j['image_url'],j['name'],"NULL","NULL",j['source_url']))+", "
+                        query7 = query7+str((uid,str(timestamp),str(j['ca_type']),str(j['fbid']),str(j['image_url']),str(j['name'].encode('utf-8')),"NULL","NULL",str(j['source_url'])))+", "
 		if test!=query7:
 			query7 = query7[:-2]+";"
 #			queries.append(query7)
@@ -113,11 +116,13 @@ for i in users:
                 survey['Error']
                 print "insert in Error table"
         except KeyError:
-		 query8 = query+"USER_SURVEY (USER_ID,RESPONSE) VALUE " + str((uid,survey))+";"
-#		 queries.append(query8)
+		survey = ast.literal_eval(json.dumps(survey))
+		survey = str(survey).replace("'",'"')
+		query8 = query+"USER_SURVEY (USER_ID,shot_timestamp,SURVEY) VALUE " + str((uid,str(timestamp),survey))+";"
+#		queries.append(query8)
 
 #	print len(queries)
-	print queries
+#	print queries
 	for q in queries:
 		print "I wash ehre"
 		cursor.execute(q)
